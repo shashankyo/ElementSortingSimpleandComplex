2245b6b2d1e1a83287af3466095fda1b9aaba4bf
shashank <shashank944533@gmail.com>
2023-01-31 20:09:46 +0530

    sorting using TreeSet simple object

---
+++ sortingofsimpleobject/src/sortingofsimpleobject/Program.java
@@ -0,0 +1,15 @@
+package sortingofsimpleobject;
+
+import java.util.TreeSet;
+
+public class Program {
+public static void main(String[] args) {
+	TreeSet ts = new TreeSet();
+	ts.add(30);
+	ts.add(40);
+	ts.add(20);
+	ts.add(10);
+	ts.add(50);
+	System.out.println(ts);
+}
+}
